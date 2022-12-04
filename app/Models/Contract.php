d2123873e90e926b33fb12ec50cf0e1ddfbd0d09
Zayd Skaff <z.a.skaf@gmail.com>
2022-12-04 13:11:24 +0200

    edit materials info

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -41,25 +41,55 @@ class Contract extends Model
 
     public function materialAmounts(){
         return $this->morphMany(MaterialAmount::class,'parentable');
-        return 'hello';
     }
 
   public function summed(){
     $subs = Subcontract::where('contract_id',$this->id)->pluck('id')->toArray();
-    $old = ContractMaterial::where([['contract_material',1],['contract_id',$this->id]])->pluck('id')->toArray();
-    $new = ContractMaterial::where([['contract_material',0],['contract_id',$this->id]])->pluck('id')->toArray();
+    $oldMaterial = ContractMaterial::where([['contract_material',1],['contract_id',$this->id]])->get();
+    $old = $oldMaterial->pluck('id');
+    $newMaterial = ContractMaterial::where([['contract_material',0],['contract_id',$this->id]])->get();
+    $new = $newMaterial->pluck('id');
+    //return $newMaterial;
     $old_amounts = [];
     
     $old_amounts = MaterialAmount::whereIn('material_id',$old)->groupBy('material_id')
          ->selectRaw('SUM(`quantity`) as quantity, 
          SUM(`not_used_quantity`) as not_used_quantity,
         `material_id` as material_id,
+        
         `individual_price` as individual_price,
         `overall_price` as overall_price'
         )
-        ->get();
+        ->get()->toArray();
+
+    $old_amounts = array_map(function($amount){
+        $material = ContractMaterial::where('id',$amount['material_id'])->first();
+        return [
+            'id' => $material['id'],
+            'number' => $material['number'],
+            'material_name' => $material['material_name'],
+            'unit' => $material['unit'],
+            'quantity' => $amount['quantity'],
+            'not_used_quantity' => $amount['not_used_quantity'],
+            'sub_contract_number' => '0'
+        ];
+    },$old_amounts);
+    $new_amounts = [];
+    foreach($newMaterial as $material){
+        $details = MaterialAmount::where('material_id',$material->id)->first();
+        $parent = $details->parentable;
+        array_push($new_amounts,[
+            'id' => $material->id,
+            'number' => $material->number,
+            'material_name' => $material->material_name,
+            'unit' => $material->unit,
+            'quantity' => $details['quantity'],
+            'not_used_quantity' => $details['not_used_quantity'],
+            'sub_contract_number' => $parent->number
+        ]);
+    }
     
-    $new_amounts = MaterialAmount::whereIn('material_id',$new)->get();
+    
     return [
         'contract' => $old_amounts,
         'other' => $new_amounts
